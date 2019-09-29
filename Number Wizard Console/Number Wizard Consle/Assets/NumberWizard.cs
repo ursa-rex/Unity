911e22d5711e4e2a8d5e7b0d9f152740e091f691
James Bryant <ursa-rex@users.noreply.github.com>
2019-09-29 16:20:33 -0400

    Test Commit

---
+++ Number Wizard Console/Number Wizard Consle/Assets/NumberWizard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = 100;
+        int min = 1;
+
+        Debug.Log("Welcome to number wizard, dude");
+        Debug.Log("Pick a number between " + min + " and " + max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
